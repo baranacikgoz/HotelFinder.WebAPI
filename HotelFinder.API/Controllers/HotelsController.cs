33155f5364bcc08a1c128f78ad84b4dd3ba90020
baranacikgoz <baran-acikgoz@outlook.com>
2022-08-16 15:11:37 +0300

    added documentation to swagger

--- HotelFinder.API/Controllers/HotelsController.cs
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -16,30 +16,54 @@ namespace HotelFinder.API.Controllers
             _hotelService = hotelService;
         }
 
+        /// <summary>
+        /// Get All Hotels.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<Hotel> Get()
         {
             return _hotelService.GetAllHotels();
         }
 
+        /// <summary>
+        /// Get Hotel by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public Hotel Get(int id)
         {
             return _hotelService.GetHotelById(id);
         }
 
+        /// <summary>
+        /// Post a new Hotel.
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPost]
         public Hotel Post([FromBody]Hotel hotel)
         {
             return _hotelService.CreateHotel(hotel);
         }
 
+        /// <summary>
+        /// Update an Hotel.
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPut]
         public Hotel Put([FromBody] Hotel hotel)
         {
             return _hotelService.UpdateHotel(hotel);
         }
 
+        /// <summary>
+        /// Delete an Hotel by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public Hotel Delete(int id)
         {
